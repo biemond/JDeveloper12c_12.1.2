89d1838495779a3ea4eae394908f6fc03d6bcd8f
Edwin Biemond <biemond@gmail.com>
2013-08-26 19:35:16 +0200

    small fixes

--- RestFulOWSM/WebService/src/nl/amis/hr/wadl/HrWadlGeneratorConfig.java
+++ RestFulOWSM/WebService/src/nl/amis/hr/wadl/HrWadlGeneratorConfig.java
@@ -5,9 +5,6 @@ import com.sun.jersey.api.wadl.config.WadlGeneratorDescription;
 import com.sun.jersey.server.wadl.generators.WadlGeneratorApplicationDoc;
 import com.sun.jersey.server.wadl.generators.WadlGeneratorGrammarsSupport;
 import com.sun.jersey.server.wadl.generators.resourcedoc.WadlGeneratorResourceDocSupport;
-import com.sun.research.ws.wadl.Grammars;
-import com.sun.research.ws.wadl.Include;
-import com.sun.research.ws.wadl.ObjectFactory;
 
 import java.util.List;
 
@@ -15,22 +12,16 @@ public class HrWadlGeneratorConfig extends WadlGeneratorConfig {
 
     @Override
     public List<WadlGeneratorDescription> configure() {
-        ObjectFactory obj = new ObjectFactory()  ;  
-        Grammars gram = obj.createGrammars();
-        Include e = obj.createInclude();
-        e.setHref("schema.xsd");
-        gram.getInclude().add(e);
         
-        WadlGeneratorConfigDescriptionBuilder builder = generator(WadlGeneratorApplicationDoc.class)
+        WadlGeneratorConfigDescriptionBuilder builder = 
+            generator(WadlGeneratorApplicationDoc.class)
         .prop( "applicationDocsStream", "application-doc.xml" )
         .generator( WadlGeneratorGrammarsSupport.class )
         .prop( "grammarsStream", "application-grammars.xml" )
         .generator( WadlGeneratorResourceDocSupport.class )
         .prop( "resourceDocStream", "resourcedoc.xml" );
-        
+
         return builder.descriptions();
-
     }
-
 }
 
